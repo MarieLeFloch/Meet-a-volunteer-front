72fb84a949045757e52ebcd9439e0de7a1100dd3
ArslaneMessaoudi <arslane.messaoudi@gmail.com>
2022-07-19 16:50:13 +0200

    Testcarousel

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,30 @@
 // == Import
+import { Carousel } from 'react-responsive-carousel';
 import reactLogo from './react-logo.svg';
-import './styles.css';
+import './style.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import image1 from '../../assets/image/1.jpg';
+import image2 from '../../assets/image/2.jpg';
+import image3 from '../../assets/image/3.jpg';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      <Carousel>
+        <div>
+          <img src={image1} />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src={image2} />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src={image3} />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
