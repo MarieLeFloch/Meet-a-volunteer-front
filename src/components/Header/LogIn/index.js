b161d6d648dda0e1038ecf7bf01a8790ae598c86
MarieLeFloch <marie-le.floch@hotmail.fr>
2022-07-20 15:43:59 +0200

    formulaire de connexion ok

--- src/components/Header/LogIn/index.js
+++ src/components/Header/LogIn/index.js
@@ -2,13 +2,29 @@
 // Commun à toutes les pages - Lorsque l'utilisateur n'est pas connecté
 // == Import
 import './style.scss';
-import { Button } from 'semantic-ui-react'
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+
 
 
 function LogIn() {
   return (
     <div className="logIn">
       <Button className="logIn__button">Login</Button>
+
+      <Form className="logIn__form">
+        <Form.Field>
+          <label>Email</label>
+          <input placeholder='Email' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input placeholder='Password' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Stay connected' />
+        </Form.Field>
+        <Button type='submit' className="logIn__form__button">Login</Button>
+      </Form>
     </div>
   );
 }
