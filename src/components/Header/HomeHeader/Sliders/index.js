083659f5a3cc7a428342faf2de186861e249a230
MarieLeFloch <marie-le.floch@hotmail.fr>
2022-07-23 12:24:32 +0200

    Add some picture - slider home

--- src/components/Header/HomeHeader/Sliders/index.js
+++ src/components/Header/HomeHeader/Sliders/index.js
@@ -5,8 +5,10 @@ import { Carousel } from 'react-responsive-carousel';
 import './style.scss';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import image1 from '../../../../assets/image/1.jpg';
-import image2 from '../../../../assets/image/2.jpg';
-import image3 from '../../../../assets/image/3.jpg';
+import image2 from '../../../../assets/image/4.jpg';
+import image3 from '../../../../assets/image/7.jpg';
+import image4 from '../../../../assets/image/8.jpg';
+import image5 from '../../../../assets/image/6.jpg';
 
 
 function Sliders() {
@@ -21,6 +23,12 @@ function Sliders() {
         </div>
         <div>
           <img src={image3} alt="" />
+        </div>
+        <div>
+          <img src={image4} alt="" />
+        </div>
+        <div>
+          <img src={image5} alt="" />
         </div>
       </Carousel>
     </div>
