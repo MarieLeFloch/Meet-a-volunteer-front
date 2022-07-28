673cc028818b38bddbaf4cd60a867c6d159d587b
MarieLeFloch <marie-le.floch@hotmail.fr>
2022-07-28 17:30:31 +0200

    dynamization of experiences - home okgit add .

--- src/components/Home/HomeContent/CardList/Card/index.js
+++ src/components/Home/HomeContent/CardList/Card/index.js
@@ -4,43 +4,36 @@ import { Card, Image, Button } from 'semantic-ui-react';
 import ImageTest from '../../../../../assets/image/1.jpg';
 import UserImageDefault from '../../../../../assets/image/user-default.png';
 
-function ExperienceCard() {
+function ExperienceCard({country, feedback, picture, title, user, createdAt}) {
+  console.log(country );
   return (
-    <div>TEST</div>
+    <Card className="experience__card">
+      <Image src={ImageTest} wrapped ui={false} />
+      <Image className="user__image" src={UserImageDefault} size="tiny" />
+      <Card.Content>
+        <Card.Meta>
+          <span className="experience__country">{country}</span>
+        </Card.Meta>
+        <Card.Meta>
+          <span className="author__name">{user.pseudo}</span>
+        </Card.Meta>
+        <Card.Header className="experience__title">{title}</Card.Header>
+        <Card.Description className="experience__preview">
+          Soufflé cake chocolate oat cake powder icing pie brownie powder. Donut fruitcake jelly-o ...
+        </Card.Description>
+        <Button
+          className="button__experience__details"
+        >
+          See more
+        </Button>
+      </Card.Content>
+      <Card.Content extra>
+        <Card.Meta>
+          <span className="uploaded__date">Uploaded the {createdAt}</span>
+        </Card.Meta>
+      </Card.Content>
+    </Card>
   );
- }
-
-
-// function ExperienceCard({country, feedback, picture, title, user, createdAt}) {
-//   console.log(country );
-//   return (
-//     <Card className="experience__card">
-//       <Image src={ImageTest} wrapped ui={false} />
-//       <Image className="user__image" src={UserImageDefault} size="tiny" />
-//       <Card.Content>
-//         <Card.Meta>
-//           <span className="experience__country">{country}</span>
-//         </Card.Meta>
-//         <Card.Meta>
-//           <span className="author__name">{user.pseudo}</span>
-//         </Card.Meta>
-//         <Card.Header className="experience__title">{title}</Card.Header>
-//         <Card.Description className="experience__preview">
-//           Soufflé cake chocolate oat cake powder icing pie brownie powder. Donut fruitcake jelly-o ...
-//         </Card.Description>
-//         <Button
-//           className="button__experience__details"
-//         >
-//           See more
-//         </Button>
-//       </Card.Content>
-//       <Card.Content extra>
-//         <Card.Meta>
-//           <span className="uploaded__date">Uploaded the {createdAt}</span>
-//         </Card.Meta>
-//       </Card.Content>
-//     </Card>
-//   );
-// }
+}
 
 export default ExperienceCard;
--- src/components/Home/HomeContent/CardList/index.js
+++ src/components/Home/HomeContent/CardList/index.js
@@ -2,16 +2,19 @@
 import './style.scss';
 import { Card } from 'semantic-ui-react';
 import ExperienceCard from './Card';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function CardList() {
+  const randomExperiencesList = useSelector((state) => state.experiences.homeExperiences)
+  // console.log(randomExperiencesList);
+
+
   return (
     <div className='experience__list'>
-      <ExperienceCard>
-      </ExperienceCard>
-      <ExperienceCard>
-      </ExperienceCard>
-      <ExperienceCard>
-      </ExperienceCard>
+        {randomExperiencesList.map((experience) => (
+          <ExperienceCard key={experience.id} {...experience} />
+        ))}
     </div>
   );
 }
