c891068fecfe1602827242934ff825f62d357a8f
ArslaneMessaoudi <arslane.messaoudi@gmail.com>
2022-08-04 12:32:09 +0200

    Fixed logged picture

--- src/components/Header/ProfileHeader/index.js
+++ src/components/Header/ProfileHeader/index.js
@@ -24,7 +24,7 @@ function ProfileHeader() {
   return (
     <div className="home__profile">
       <Link to={`/volunteers/${userPseudo}`}><Image src={`http://romain2518-server.eddi.cloud/images/pp/${profileUserPicture}`} avatar size="mini" className="home__profile--avatar" onClick={handleClickProfile} /></Link>
-      <span className="home__profile--pseudo"><Link to={`/volunteers/${userPseudo}`}>{userPseudo}</Link> </span>
+      <span className="home__profile--pseudo"><Link onClick={handleClickProfile} to={`/volunteers/${userPseudo}`}>{userPseudo}</Link> </span>
       <Button className="home__profile--button" onClick={handleClick}>Logout</Button>
     </div>
   );
--- src/components/Profile/ProfileContent/ExperienceList/Experience/index.js
+++ src/components/Profile/ProfileContent/ExperienceList/Experience/index.js
@@ -1,11 +1,12 @@
 // Imports
 import './style.scss';
 import { Image, Button, Icon } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import photo from '../../../../../assets/image/9.jpg';
+import { saveExperienceId } from '../../../../../actions/experience';
 
-function Experience({ id, title, picture }) {
+function Experience({ id, title, picture, slugTitle }) {
   // Verif si profil personnel
   const { pseudo } = useSelector((state) => state.profile.profileDetails);
   const { userPseudo } = useSelector((state) => state.user.login);
@@ -13,16 +14,21 @@ function Experience({ id, title, picture }) {
   if (pseudo == userPseudo) {
     isOwnProfile = true;
   }
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(saveExperienceId(id));
+  }
 
   return (
     <div className="profile__experience">
       <div className="profile__experience--image">
-        <Image src={photo} size="medium" />
+        <Image src={`http://romain2518-server.eddi.cloud/images/experiencePicture/${picture}`} size="medium" />
       </div>
       <div className="profile__experience--text">
         <h3>{title}</h3>
         <p>Lemon drops biscuit jelly beans croissant dessert tart tiramisu. Soufflé bear claw fruitcake cheesecake biscuit sweet candy. Biscuit chocolate bar pudding marshmallow brownie powder cupcake dragée. Jelly muffin chocolate cake fruitcake cookie sweet roll toffee brownie. Lemon drops biscuit jelly beans croissant dessert tart tiramisu. Soufflé bear claw fruitcake cheesecake biscuit sweet candy. Biscuit chocolate bar pudding marshmallow brownie powder cupcake dragée. Jelly muffin chocolate cake fruitcake cookie sweet roll toffee brownie.</p>
-        <div><a>read more</a></div>
+        <div><Link onClick={handleClick} to={`/experiences/${id}/${slugTitle}`}>Read More</Link></div>
       </div>
       {isOwnProfile
       && (
